556f4df13d5e796d8ec050b1e7d6051ae1d4c398
AzharKoivila <azharkoivila@gmail.com>
2025-02-27 14:55:32 +0530

    implimented quary Serach and pagination

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,22 @@ var express = require('express');
 var router = express.Router();
 const Products = require('../models/products');
 
-// GET /api/v1/
-router.get('/', async function(req, res, next) {
-  // const response = await Products.find({});
-  // res.status(200).json(response);
-  throw new Error("This is an error");
-  
-
+// get all products
+router.get('/products', async function(req, res, next) {
+  const {title, category, price} = req.query;             //for now only these three query params are  used we can add more by use case
+  const query = {};
+  // if query params are present then add them to query object
+  if(title) query.title = title;
+  if(category) query.category = category;
+  if(price) query.price = price;
+  let response = Products.find(query);
+  // pagination
+  const page= parseInt(req.query.page) || 1;
+  const skip = (page- 1) * 10;
+  response = response.skip(skip).limit(10);
+  const Product = await response;
+  console.log(Product.length);
+  res.status(200).json(Product);
 });
 
 module.exports = router;
